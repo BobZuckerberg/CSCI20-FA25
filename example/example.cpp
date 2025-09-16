ff8b5777ae64e737924a7256b12dd382f222a0a6
BobZuckerberg <drussell009@student.butte.edu>
2025-09-16 20:14:32 +0000

    Uh yeah I made the example on that day, learning more stuff and printing

---
+++ example/example.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+    int x = 1;
+    bool b = true;
+    std::printf("%i", b);
+    //std::printf("hi hi world");
+    //std::printf("%i", x);
+    return 0;
+}
