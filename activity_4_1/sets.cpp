63392bb638d822614d8304b0f72f5eb446181b89
BobZuckerberg <drussell009@student.butte.edu>
2025-09-25 16:15:27 +0000

    comments and vectors

--- activity_4_1/sets.cpp
+++ activity_4_1/sets.cpp
@@ -13,14 +13,17 @@ int main() {
     printf("%i", a1[0]); /*printf's, with the integer command, "%i", from the array a1, element 0 in that list.*/
     printf("%i,", a2[2]); 
     printf("\n"); /*printf's a linebreak*/
-    printf("%i", a3[1]);
-    std::vector<int> v1 = {1, 2,};
-    printf("%i",v1[0]);
-    printf("\n"); 
-    printf("%c", squid.at(1));
-    printf("\n"); 
-    printf("%s\n", squid_str.at(0).c_str());
-    printf()
+    printf("%i", a3[1]); /* printf's, expecting an integer, the element at the 1 place in a3*/
+    std::vector<int> v1 = {1, 2,}; /*creates a vector, holding integers, named v1, holding 1 and 2*/
+    std::vector<std::string> v2 = {"ababa", "awawa",}; /*creates a vector, holding strings, named v2, holding ababa and awawa*/
+    std::vector<int> v3 = {0, 0, 1, 1, 5, 9}; /*creates a vector, holding integers, named v1, holding 0,0,1,5,9*/
+    printf("%i",v1[0]); /*printf's, expecting an integer, the zeroth element in v1*/
+    printf("\n"); /*refer to line 15*/
+    printf("%c", squid.at(1)); /*printfs, expecting a single character, from the array named squid, at position 1*/
+    printf("\n"); /*refer to line 15*/
+    printf("%s\n", squid_str.at(0).c_str()); /*printf's, expecting a string, from squid_str, at the zeroth position. It also makes a linebreak, and converts it into a c_string from a basic_string.*/
+    printf("%s\n", v2.at(1).c_str());
+    printf("%i", v3.at(2));
 
     return 0;
 }
